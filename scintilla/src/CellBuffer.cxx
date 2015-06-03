e3fbebf4cd15f665698ef8c6388e8edc88cd0124
Masabho <masabhopeter@gmail.com>
2015-06-03 17:44:09 -0500

    add_stupid_line
    foobar

--- scintilla/src/CellBuffer.cxx
+++ scintilla/src/CellBuffer.cxx
@@ -4,7 +4,7 @@
  **/
 // Copyright 1998-2001 by Neil Hodgson <neilh@scintilla.org>
 // The License.txt file describes the conditions under which this software may be distributed.
-
+dgggsdsg
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
